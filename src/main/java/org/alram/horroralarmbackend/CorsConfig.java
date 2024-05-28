31b5e8523806b4d119eab2b26200c3800084a95e
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-05-28 19:58:42 +0900

    refactor: CORS에 주입하는 파라미터를 환경변수에서 읽어오게 바꾸었다.

--- src/main/java/org/alram/horroralarmbackend/CorsConfig.java
+++ src/main/java/org/alram/horroralarmbackend/CorsConfig.java
@@ -1,17 +1,26 @@
 package org.alram.horroralarmbackend;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
+    @Value("${cors.allowed.origins}")
+    private String allowedOrigins;
+    @Value("${cors.allowed.headers}")
+    private String allowedHeaders;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // 허용할 Origin
-            .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP Method
+            .allowedHeaders(allowedHeaders) // 허용할 Header
+            .allowedOrigins(allowedOrigins) // 허용할 Origin
+            .allowedMethods("GET") // 허용할 HTTP Method
             .allowCredentials(true) // 클라이언트에서 쿠키 전송 허용
             .maxAge(3600); // 프리플라이트 요청 캐싱 시간 (초 단위)
     }
