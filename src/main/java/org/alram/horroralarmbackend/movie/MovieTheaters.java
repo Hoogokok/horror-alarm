1c61035e41b131a1c8731b0b9c6e042709f105f4
Hoogokok <85615666+Hoogokok@users.noreply.github.com>
2024-06-13 08:03:35 +0900

    refactor: 중간 엔티티인 MovieTheaters에 영화 아이디와 극장 아이디 필드에 칼럼을 찾을 수 있게 JoinColumn 어노테이션 추가

--- src/main/java/org/alram/horroralarmbackend/movie/MovieTheaters.java
+++ src/main/java/org/alram/horroralarmbackend/movie/MovieTheaters.java
@@ -1,9 +1,11 @@
 package org.alram.horroralarmbackend.movie;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -18,8 +20,10 @@ public class MovieTheaters {
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "movie_id")
     private Movie movieId;
     @ManyToOne
+    @JoinColumn(name = "theaters_id")
     private Theaters theatersId;
 
     public MovieTheaters() {
